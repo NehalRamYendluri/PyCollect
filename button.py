b84350a97d247847a638bf038c09f9c91a47e8b1
NehalRamYendluri <88031152+NehalRamYendluri@users.noreply.github.com>
2023-05-20 10:43:49 +0530

    Update button.py

--- button.py
+++ button.py
@@ -7,9 +7,9 @@ class Button():
         self.text = text
         self.width = width
         self.height = height
-        self.font = pygame.font.Font(pygame.font.get_default_font(), 18) 
+        self.font = pygame.font.Font(pygame.font.get_default_font(), 27) 
         self.text1 = pygame.font.Font.render(self.font,self.text,True,(0,0,0),bg)
-        self.rect = pygame.Rect(self.x,self.y,width,height)
+        self.rect = pygame.Rect(self.x,self.y,self.width,self.height)
         self.bg = bg
         if img != None:
           self.img = pygame.transform.scale(img,(self.width,self.height))
@@ -19,13 +19,15 @@ class Button():
             self.img= None      
     def draw(self,win):
         if self.img != None:
-           win.surface.blit(self.img,dest=(self.x,self.y)) 
+           win.blit(self.img,dest=(self.x,self.y)) 
         else:   
-           pygame.draw.rect(win.surface,self.bg,self.rect)
-           win.surface.blit(self.text1,dest=(self.x,self.y))
-    def click(self,event):
+           pygame.draw.rect(win,self.bg,self.rect)
+           win.blit(self.text1,dest=(self.x,self.y))
+    def click(self,event,x,y):
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            pos = pygame.mouse.get_pos()
+            pos = list(pygame.mouse.get_pos())
+            pos[0] -= x
+            pos[1] -= y
             if self.rect.collidepoint(pos):
                 return True
         return False           
